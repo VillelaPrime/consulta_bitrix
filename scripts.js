221dd638684ec8027a781bd7f44c3877a4fa0c33
VillelaPrime <159080524+VillelaPrime@users.noreply.github.com>
2025-04-25 13:40:41 +0000

    modified:   scripts.js

--- scripts.js
+++ scripts.js
@@ -42,7 +42,7 @@ document.addEventListener('DOMContentLoaded', function () {
         container.classList.remove('theme-green', 'theme-red');
 
         try {
-            const apiBase = 'https://villela.bitrix24.com.br/rest/16640/92aynyu0y6nfpobo';
+            const apiBase = 'https://villela.bitrix24.com.br/rest/16640/f9xtq3boo2lt8aa6';
             const dealListUrl = `${apiBase}/crm.deal.list.json`;
             const contactGetUrl = `${apiBase}/crm.contact.get`;
             const categoryListUrl = `${apiBase}/crm.dealcategory.list.json`;
